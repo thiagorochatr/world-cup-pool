9eeff75a96358e9e9cdb86ca50b7b4ad16b3fefa
thiagorochatr <thiagord2016@gmail.com>
2022-11-05 19:41:00 -0300

    3 - completed

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 import { NativeBaseProvider, StatusBar } from "native-base";
 
 import { Loading } from './src/components/Loading';
-import { Find } from './src/screens/Find';
+import { Pools } from './src/screens/Pools';
 
 import { AuthContextProvider } from './src/contexts/AuthContext';
 
@@ -21,7 +21,7 @@ export default function App() {
           translucent
           />
         
-        { fontsLoaded ? <Find /> : <Loading /> }
+        { fontsLoaded ? <Pools /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
